497fcc71996bc0078caecb06a80ee5cb901faae7
Szymon Siczek <szymonsiczek@gmail.com>
2020-08-01 14:23:40 +0200

    instances of class Storage aytomatically added to list in a subclass

--- StorageManager_offline_version.py
+++ StorageManager_offline_version.py
@@ -1,6 +1,8 @@
 
+
 class Storage:
     instances = []
+    subclasses = ['Sound', 'Light', 'Stage', 'Power', 'Extra']
 
     def __init__(self, subclass, name, model, quantity):
         self.subclass = subclass
@@ -8,6 +10,8 @@ class Storage:
         self.model = model
         self.quantity = quantity
         Storage.instances.append(self)
+        if self.subclass in Storage.subclasses:
+            eval(self.subclass).instances.append(self)
 
     @property
     def full(self):
@@ -19,22 +23,33 @@ class Sound(Storage):
 
 
 class Light(Storage):
+    instances = []
     pass
 
 
 class Stage(Storage):
+    instances = []
     pass
 
 
 class Power(Storage):
+    instances = []
     pass
 
 
 class Extra(Storage):
+    instances = []
     pass
 
 
-sound1 = Storage('sound', 'Mixer', 'Midas M32', 1)
-sound2 = Storage('sound', 'Stagebox', 'DL32', 1)
+gear1 = Storage('Sound', 'Mixer', 'Midas M32', 1)
+gear2 = Storage('Sound', 'Stagebox', 'DL32', 1)
+gear3 = Storage('Light', 'Beam', 'Gladiator', 16)
+gear4 = Storage('Light', 'Solar 27Q', 'Prolights', 12)
+gear5 = Storage('Stage', 'Podest 2x1m', 'Alustage', 18)
+gear6 = Storage('Power', 'Rodzielnia 32A', 'Ta w kejsie', 1)
+gear7 = Storage('Extra', 'Toolbox', '-', 1)
 
-print(Sound.instances)
+
+for i in Storage.instances:
+    print(i.full)
