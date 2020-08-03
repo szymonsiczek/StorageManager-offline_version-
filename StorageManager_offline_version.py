846fed7915a5c8392dd53bf21a39f1d48b126711
Szymon Siczek <szymonsiczek@gmail.com>
2020-08-03 18:20:28 +0200

    added Item class instead of Storage, started to work on code to make it more like OOP

--- StorageManager_offline_version.py
+++ StorageManager_offline_version.py
@@ -1,6 +1,6 @@
 
 
-class Storage:
+class Item:
     instances = []
     subclasses = ['Sound', 'Light', 'Stage', 'Power', 'Extra']
 
@@ -9,67 +9,73 @@ class Storage:
         self.name = name
         self.model = model
         self.quantity = quantity
-        Storage.instances.append(self)
-        if self.subclass in Storage.subclasses:
+        Item.instances.append(self)
+        if self.subclass in Item.subclasses:
             eval(self.subclass).instances.append(self)
 
     @property
-    def full(self):
+    def fullname(self):
         return self.name + ' ' + self.model
 
+    def create_item():
+        pass
 
-class Sound(Storage):
+    def read_items_from_file():
+        File = open(file)
+        for line in File.readlines():
+            item = line.split(', ')
+            if item[0] in Item.subclasses:
+                eval(item[0])(item[0], item[1], item[2], int(item[3]))
+
+    def add_item_to_item_file(string):
+        File = open(file, 'a')
+        File.write('\n' + string)
+        File.close()
+        item = string.split(', ')
+        if item[0] in Item.subclasses:
+            eval(item[0])(item[0], item[1], item[2], int(item[3]))
+
+    def delete_item_from_file(file):
+        pass
+
+    def show_all_instances_of_subclass(subclass):
+        pass
+
+    def show_all():
+        for item in Item.instances:
+            print(item.fullname)
+
+
+class Sound(Item):
     instances = []
 
 
-class Light(Storage):
+class Light(Item):
     instances = []
-    pass
 
 
-class Stage(Storage):
+class Stage(Item):
     instances = []
-    pass
 
 
-class Power(Storage):
+class Power(Item):
     instances = []
-    pass
 
 
-class Extra(Storage):
+class Extra(Item):
     instances = []
-    pass
 
 
-def read_gear_from_file():
-    File = open(file)
-    for line in File.readlines():
-        gear = line.split(', ')
-        if gear[0] in Storage.subclasses:
-            eval(gear[0])(gear[0], gear[1], gear[2], int(gear[3]))
+def starting_program():
+    print('What would you like to do? Type a number. \n\n')
+    show_options_and_get_user_choice()
 
 
-def add_item_to_gear_file(string):
-    File = open(file, 'a')
-    File.write('\n' + string)
-    File.close()
-    gear = string.split(', ')
-    if gear[0] in Storage.subclasses:
-        eval(gear[0])(gear[0], gear[1], gear[2], int(gear[3]))
+def show_actions_options_and_get_user_choice():
+    # Work on that! Do you need to get show actions, get choice and eval() actions in the same func? If so, change the name of this func.
+    '1. Add item\n2. Show all items\n3. Show list of items from a certain category\n4. Delete item\n5.Delete all items\n\n'
+    action_options = {}
+    choice = input()
 
 
-def delete_item_from_file(file):
-    pass
-
-
-def show_all_instances_of_subclass(subclass):
-    pass
-
-
-def show_all():
-    for item in Storage.instances:
-        print(item.full)
-
-
-file = 'C:\\Users\\User\\Dropbox\\Python\\my_projects\\storageManager\\gear_list.txt'
+file = 'C:\\Users\\User\\Dropbox\\Python\\my_projects\\storageManager\\item_list.txt'
